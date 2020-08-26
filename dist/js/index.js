71bdf65caa2015e8ee90cc03bec582f9ba800aa9
gagik <gagik.n@gmail.com>
2020-08-26 14:02:07 +0200

    Add carousels with swiper and new button styling, still some bugs need to be fixed

--- dist/js/index.js
+++ dist/js/index.js
@@ -3,49 +3,95 @@
 require('dotenv');
 const $ = require('jquery');
 
-const config = {
-  type: 'carousel',
-  startAt: 0,
-  perView: 3,
-  autoplay: 10000,
-  animationDuration: 800,
-  animationTimingFunc: 'ease-in',
-  breakpoints: {
-    1024: {
-      perView: 2,
-    },
-    600: {
-      perView: 1,
-    },
-  },
-  peek: {
-    before: 50,
-    after: 20,
-  },
-};
-new Glide('.glide', config).mount();
-
 //
 $('#navBtn').click(function () {
-  $('#navBtn').toggleClass('close');
-  $('ul').toggleClass('hide-navbar');
-  $('.blocker').toggleClass('on');
+	$('#navBtn').toggleClass('close');
+	$('ul').toggleClass('hide-navbar');
+	$('.blocker').toggleClass('on');
 });
 //
 // closing Menu  with outside click
 //  div with class of blocker added in order to maintain outside click event
 // when the menu is open
 $('.blocker').click(() => {
-  if ($('#navBtn').hasClass('close')) {
-    toggleMenu();
-    $('.blocker').toggleClass('on');
-  }
+	if ($('#navBtn').hasClass('close')) {
+		toggleMenu();
+		$('.blocker').toggleClass('on');
+	}
 });
 
 function toggleMenu() {
-  $('#navBtn').toggleClass('close');
-  $('ul').toggleClass('hide-navbar');
+	$('#navBtn').toggleClass('close');
+	$('ul').toggleClass('hide-navbar');
 }
+
+//
+// Swiper Carousel
+const Swiper = require('../../node_modules/swiper/swiper-bundle');
+
+const mySwiper = new Swiper(' #partner', {
+	roundLengths: true,
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	direction: 'horizontal',
+	slidesPerView: '4',
+	speed: 800,
+	loop: true,
+	grabCursor: true,
+	spaceBetween: 50,
+	freeMode: true,
+	centeredSlides: true,
+	breakpoints: {
+		1024: {
+			slidesPerView: 3,
+			spaceBetween: 20,
+		},
+		600: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+		},
+		200: {
+			slidesPerView: 1,
+			spaceBetween: 20,
+		},
+	},
+});
+
+const mySwiper1 = new Swiper('#pop-trip-cards', {
+	direction: 'horizontal',
+	loop: false,
+	slidesPerView: 4,
+	spaceBetween: 20,
+	breakpoints: {
+		1024: {
+			slidesPerView: 3,
+			spaceBetween: 10,
+		},
+		800: {
+			slidesPerView: 2,
+			spaceBetween: 5,
+		},
+		200: {
+			slidesPerView: 2,
+			spaceBetween: 5,
+		},
+	},
+	grabCursor: true,
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'bullets',
+	},
+});
+
+//
 //get data from OpenWeather API Function
 //
 
@@ -53,13 +99,13 @@ const ApiID = process.env['unsplash_Api'];
 const baseURL = `https://api.unsplash.com/search/collections?page=2&query=travel&client_id=${ApiID}`;
 
 async function getData(url) {
-  const respond = await fetch(url);
-  try {
-    const data = await respond.json();
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.error('error', error);
-  }
+	const respond = await fetch(url);
+	try {
+		const data = await respond.json();
+		console.log(data);
+		return data;
+	} catch (error) {
+		console.error('error', error);
+	}
 }
 // getData(baseURL);
